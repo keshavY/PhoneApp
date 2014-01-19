eb8125bfc91d2fd2bc16619bac3c7b0002a733b2
keshavY <keshavyadav87@gmail.com>
2014-01-19 19:29:03 +0530

    select layout is added

--- PhoneApp/index.js
+++ PhoneApp/index.js
@@ -6,11 +6,18 @@
     // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
     // DevExpress.devices.current({ platform: "generic" });
 
-    $(function() {
+    $(function () {
+        //overide navigation
+        //MyApp.myController = new myLayoutController({ layoutTemplateName: "myLayout" });
+        //DevExpress.framework.html.layoutControllers.push({
+        //    navigationType: "myControllerNavigation",
+        //    controller: MyApp.myController
+        //});
+
+        //default navigation
         MyApp.app = new DevExpress.framework.html.HtmlApplication({
-            namespace: MyApp,
-            
-            navigationType: "navbar",
+            namespace: MyApp,            
+            navigationType: 'navbar',
             navigation: [
               {
                   title: "Login",
@@ -32,6 +39,17 @@
         });
         
         MyApp.app.router.register(":view", { view: "home" });
+        //MyApp.app.router.register(":view/:name", { view: "home", name: '' });
+        //MyApp.app.resolveLayoutController.add(function (args) {
+        //    var viewName = args.viewInfo.viewName;
+        //    if (viewName === "select") {
+        //        var result = $.grep(args.availableLayoutControllers, function (item, index) {
+        //            return item.navigationType == 'empty';
+        //        });
+        //        result.length ? result[0].controller : null;
+        //        args.layoutController = result;
+        //    }
+        //});
         MyApp.app.navigate();
     });
     
