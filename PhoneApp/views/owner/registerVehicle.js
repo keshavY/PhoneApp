b97826f41f42c93dd2fbe5dcba51030940638570
keshavY <keshavyadav87@gmail.com>
2014-01-28 23:50:52 +0530

    owner layouts added.

---
+++ PhoneApp/views/owner/registerVehicle.js
@@ -0,0 +1,11 @@
+﻿
+var tabs = [
+{ text: "REGISTER VEHICLE", iconSrc: "css/images/search.png" },
+{ text: "PENDING APPROVAL", iconSrc: "css/images/status_unknown.png" }
+];
+var tabClicked = function (e) {
+    if (e.itemData.text == "REGISTER VEHICLE")
+        MyApp.app.navigate("registerVehicle", { root: "true" });
+    if (e.itemData.text == "PENDING APPROVAL")
+        MyApp.app.navigate("requestPending", { root: "true" });
+}
